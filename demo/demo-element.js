b5307eac29ef5a0d5e190368be60797a1a04946c
Flor Urbinati <flor29600@hotmail.com>
2020-02-23 19:16:31 +0100

    tutorial completed

--- demo/demo-element.js
+++ demo/demo-element.js
@@ -7,13 +7,18 @@ class DemoElement extends PolymerElement {
     return html`
       <style>
         :host {
-          font-family: sans-serif;
+          font-family: monospace;
+          --icon-toggle-fill: white;
+    --icon-toggle-stroke:#222;
+    --icon-pressed-fill: crimson;
         }
       </style>
       
       <h3>Statically-configured icon-toggles</h3>
-      <icon-toggle toggle-icon="star"></icon-toggle>
       <icon-toggle toggle-icon="star" pressed></icon-toggle>
+      <icon-toggle toggle-icon="menu"></icon-toggle>
+      <icon-toggle toggle-icon="settings"></icon-toggle>
+      <icon-toggle toggle-icon="add" pressed></icon-toggle>
         
       <h3>Data-bound icon-toggle</h3>
       <!-- use a computed binding to generate the message -->
--- icon-toggle.js
+++ icon-toggle.js
@@ -1,26 +1,54 @@
-import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
-import '@polymer/iron-icon/iron-icon.js';
+import { PolymerElement, html } from "@polymer/polymer/polymer-element.js";
+import "@polymer/iron-icon/iron-icon.js";
 
 class IconToggle extends PolymerElement {
   static get template() {
     return html`
       <style>
         /* shadow DOM styles go here */
-        span {
-          color: blue;
-        }
         :host {
           display: inline-block;
         }
+        :host([pressed]) iron-icon {
+          fill: var(--icon-pressed-fill, currentcolor);
+        }
+        iron-icon {
+          fill: var(--icon-toggle-fill, rgba(0, 0, 0, 0));
+          stroke: var(--icon-toggle-stroke, currentcolor);
+          stroke-width: 1px;
+        }
+        iron-icon:hover{
+          cursor: pointer;
+        }
       </style>
-  
-      <!-- shadow DOM goes here -->
-      <span>Not much here yet.</span>
+
+
+      <iron-icon icon=[[toggleIcon]] on-click='toggle'></iron-icon>
     `;
   }
-  constructor() {
-    super();
+
+  static get properties(){
+    return {
+      toggleIcon:{
+        type: String
+      },
+      pressed:{
+        type: Boolean,
+        value: false,
+        notify: true,
+        reflectToAttribute: true
+      }
+    }
+  }
+
+  constructor(){
+    super(),
+    this.addEventListener('click', this.toggleAttribute.bind(this))
+  }
+
+  toggle(){
+    this.pressed = !this.pressed
   }
 }
 
-customElements.define('icon-toggle', IconToggle);
+customElements.define("icon-toggle", IconToggle);
